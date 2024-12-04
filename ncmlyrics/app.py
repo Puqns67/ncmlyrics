Puqns67/ncmlyrics

commit 4c01aa7467496aa6dc98cc5735dc1ad2935a9120

refactor: convert Enum to StrEnum and reduce some duplicated code

ncmlyrics/object.py
@@ -157,20 +157,11 @@ class NCMLyrics:
 
         lyrics: dict[LrcType, str] = {}
 
-        try:
-            lyrics[LrcType.Origin] = data["lrc"]["lyric"]
-        except KeyError:
-            pass
-
-        try:
-            lyrics[LrcType.Translation] = data["tlyric"]["lyric"]
-        except KeyError:
-            pass
-
-        try:
-            lyrics[LrcType.Romaji] = data["romalrc"]["lyric"]
-        except KeyError:
-            pass
+        for lrctype in LrcType:
+            try:
+                lyrics[lrctype] = data[lrctype.ncmAPIString()]["lyric"]
+            except KeyError:
+                pass
 
         return cls(
             id=None,

ncmlyrics/enum.py
@@ -1,9 +1,9 @@
-from enum import Enum, auto
+from enum import StrEnum, auto
 
 __all__ = ["LrcType", "LrcMetaType", "LinkType"]
 
 
-class LrcType(Enum):
+class LrcType(StrEnum):
     Origin = auto()
     Translation = auto()
     Romaji = auto()
@@ -17,18 +17,27 @@ class LrcType(Enum):
             case LrcType.Romaji:
                 return "音"
 
+    def ncmAPIString(self) -> str:
+        match self:
+            case LrcType.Origin:
+                return "lrc"
+            case LrcType.Translation:
+                return "tlyric"
+            case LrcType.Romaji:
+                return "romalrc"
 
-class LrcMetaType(Enum):
+
+class LrcMetaType(StrEnum):
     Title = "ti"
     Artist = "ar"
     Album = "al"
     Author = "au"
     Length = "length"
-    LrcAuthor = "by"
+    Author = "by"
     Offset = "offset"
 
 
-class LinkType(Enum):
-    Track = "track"
-    Album = "album"
-    Playlist = "playlist"
+class LinkType(StrEnum):
+    Track = auto()
+    Album = auto()
+    Playlist = auto()

commit d16d14e20d008fb9e6cb896eb67d5b2b58221f9e

fix: wrong rename let values duplicate, and rename file as type to fix import error
Fixes: 4c01aa7467496aa6dc98cc5735dc1ad2935a9120 (refactor: convert Enum to StrEnum and reduce some duplicated code)

ncmlyrics/app.py
@@ -12,7 +12,7 @@ from rich.progress import Progress, TaskID
 from rich.theme import Theme
 
 from .api import NCMApi
-from .enum import LinkType
+from .type import LinkType
 from .error import ParseLinkError, UnsupportedLinkError
 from .lrc import Lrc
 from .object import NCMAlbum, NCMPlaylist, NCMTrack

ncmlyrics/lrc.py
@@ -6,7 +6,7 @@ from re import compile as compileRegex
 from typing import Generator, Iterable, Self
 
 from .constant import CONFIG_LRC_AUTO_MERGE, CONFIG_LRC_AUTO_MERGE_OFFSET
-from .enum import LrcMetaType, LrcType
+from .type import LrcMetaType, LrcType
 from .error import UnsupportedPureMusicTrackError
 from .object import NCMLyrics
 

ncmlyrics/object.py
@@ -4,7 +4,7 @@ from typing import Self
 
 from httpx import Response
 
-from .enum import LrcType
+from .type import LrcType
 from .error import ObjectParseError
 
 __all__ = ["NCMTrack", "NCMAlbum", "NCMPlaylist", "NCMLyrics"]

ncmlyrics/type.py
@@ -33,7 +33,7 @@ class LrcMetaType(StrEnum):
     Album = "al"
     Author = "au"
     Length = "length"
-    Author = "by"
+    LrcAuthor = "by"
     Offset = "offset"
 
 

ncmlyrics/util.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse as parseUrl
 
 from httpx import get as getHttp
 
-from .enum import LinkType
+from .type import LinkType
 from .error import ParseLinkError, UnsupportedLinkError
 
 __all__ = ["Link", "parseLink", "safeFileName"]

tests/test_utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from unittest import TestCase
 
-from ncmlyrics.enum import LinkType
+from ncmlyrics.type import LinkType
 from ncmlyrics.error import ParseLinkError, UnsupportedLinkError
 from ncmlyrics.object import NCMTrack
 from ncmlyrics.util import Link, parseLink, pickOutput, testExistTrackSource